5b9bcc9d8b701eb2215e6a08151f236bed95220e
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-12-18 19:20:13 +0000

    add: bottom navigation

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,43 @@
-import React from "react";
-import { Text } from "react-native";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { Ride } from "@/types/type";
+
 const Home = () => {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    signOut();
+    router.replace("/(auth)/sign-in");
+  };
+
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="bg-general-500">
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </SafeAreaView>
   );
 };
